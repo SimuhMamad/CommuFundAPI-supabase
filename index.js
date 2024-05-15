d6bcf49fe7b66826991acb10d60af581ff0e5ff8
SimuhMamad <muhammadabdurrahman891@gmail.com>
2024-05-15 14:58:07 +0700

    second commit

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ app.get("/registrasi.html", (req, res) => {
     res.sendFile(__dirname + "/public/registrasi.html");
 })
 
-app.listen(process.env.PORT, () => console.log("Server is running on port 5500"))
+app.listen(process.env.PORT, () => console.log("Server is running on port "+process.env.PORT))
